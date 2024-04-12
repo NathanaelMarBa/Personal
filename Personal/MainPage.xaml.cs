8031282c43e7cd7d648073cb7c6a76a28bf14830
NathanaelMarBa <kevin_1321124060@uptecamac.edu.mx>
2024-04-12 15:36:11 -0600

    Agregar archivos de proyecto.

---
+++ Personal/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using HTTPupt;
+
+namespace Personal
+   
+{
+    public partial class MainPage : ContentPage
+    {
+        PeticionHTTP peticion = new PeticionHTTP("https://Devnath.bsite.net/cliente/");
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Listar_Clicked (object sender, EventArgs e)
+        {
+            peticion.PedirComunicacion("listar", MetodoHTTP.GET, TipoContenido.JSON);
+            string respuestaJSON = peticion.ObtenerJson();
+            lblLista.Text = respuestaJSON;
+
+
+        }
+
+
+
+    }
+
+}
---
+++ Personal/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Personal
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
